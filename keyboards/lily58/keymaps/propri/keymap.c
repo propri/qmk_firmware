b900d6f8fdc30746bc8370320471afc345f668cd
Wilhelm Mailaender <wilhelm@mailaender-home.de>
2023-08-03 18:53:09 +0200

    q instead stat - im default layer, rlayer 3 mit y getauscht

--- keyboards/lily58/keymaps/propri/keymap.c
+++ keyboards/lily58/keymaps/propri/keymap.c
@@ -13,13 +13,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  `   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  -   |
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  [   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      | (GUI)| (ALT)| (CTL)|(SHFT)|      |                    |      |(SHFT)|(CTL) |(ALT) |(GUI) |      |
- * | CAPS |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * | CAPS |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  | NUHS |
  * |------+------+------+------+------+------|  ESC  |    |    ]  |------+------+------+------+------+------|
  * |      |      |      |      |(RALT)|      |       |    |       |      |(RALT)|      |      |      |      |
- * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |NUHS |
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  '   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt | LOWER|Del   | /Enter  /       \Space \  |BackSP| RAISE| RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -28,9 +28,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_QWERTY] = LAYOUT(
   KC_ESC,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5,                    KC_6, KC_7,         KC_8,         KC_9,         KC_0,            KC_GRV,
-  KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                    KC_Y, KC_U,         KC_I,         KC_O,         KC_P,            KC_MINS,
-  KC_CAPS, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                    KC_H, RSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_SCLN), KC_QUOT,
-  KC_LSFT, KC_Z,         KC_X,         KC_C,         RALT_T(KC_V), KC_B, KC_ESC,  KC_RBRC,  KC_N, RALT_T(KC_M), KC_COMM,      KC_DOT,       KC_SLSH,         KC_NUHS,
+  KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                    KC_Y, KC_U,         KC_I,         KC_O,         KC_P,            KC_LBRC,
+  KC_CAPS, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                    KC_H, RSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_SCLN), KC_NUHS,
+  KC_LSFT, KC_Z,         KC_X,         KC_C,         RALT_T(KC_V), KC_B, KC_ESC,  KC_RBRC,  KC_N, RALT_T(KC_M), KC_COMM,      KC_DOT,       KC_SLSH,         KC_QUOT,
                                 KC_LALT, MO(_LOWER), KC_DEL, KC_ENT,           KC_SPC, KC_BSPC, MO(_RAISE), KC_RALT
 ),
 /* LOWER
