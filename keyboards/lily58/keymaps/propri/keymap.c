a7e6af975075fb54e3618242bfd1331c97597cd3
Wilhelm Mailaender <wilhelm.mailaender@avenga.com>
2023-08-07 07:53:05 +0200

    links layer 3 auch mit nuhs
    hoffentlich besser als mit capslock

--- keyboards/lily58/keymaps/propri/keymap.c
+++ keyboards/lily58/keymaps/propri/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   KC_ESC,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5,                    KC_6, KC_7,         KC_8,         KC_9,         KC_0,            KC_GRV,
   KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                    KC_Y, KC_U,         KC_I,         KC_O,         KC_P,            KC_LBRC,
-  KC_CAPS, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                    KC_H, RSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_SCLN), KC_NUHS,
+  KC_NUHS, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                    KC_H, RSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_SCLN), KC_NUHS,
   KC_LSFT, KC_Z,         KC_X,         KC_C,         RALT_T(KC_V), KC_B, KC_DEL,  KC_RBRC,  KC_N, RALT_T(KC_M), KC_COMM,      KC_DOT,       KC_SLSH,         KC_QUOT,
                                 KC_LALT, MO(_LOWER), KC_ESC, KC_ENT,           KC_SPC, KC_BSPC, MO(_RAISE), KC_RALT
 ),
