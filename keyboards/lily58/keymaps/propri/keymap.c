700febfa77ee7ee8186c0f2344481e56e7f80d0a
Wilhelm Mailaender <wilhelm.mailaender@avenga.com>
2023-08-10 07:34:19 +0200

    Shift auf äußere Daumentasten

--- keyboards/lily58/keymaps/propri/keymap.c
+++ keyboards/lily58/keymaps/propri/keymap.c
@@ -18,12 +18,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  [   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      | (GUI)| (ALT)| (CTL)|(SHFT)|      |                    |      |(SHFT)|(CTL) |(ALT) |(GUI) |      |
+ * |      | (GUI)| (ALT)| (CTL)|      |      |                    |      |      |(CTL) |(ALT) |(GUI) |      |
  * | CAPS |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  | NUHS |
  * |------+------+------+------+------+------|  Del  |    |    ]  |------+------+------+------+------+------|
  * |      |      |      |      |(RALT)|      |       |    |       |      |(RALT)|      |      |      |      |
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |  '   |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   |      |      |      | /(SHFT) /       \(SHFT)\  |      |      |      |
  *                   | LAlt | LOWER| ESC  | /Enter  /       \Space \  |BackSP| RAISE| RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
@@ -32,9 +33,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   KC_ESC,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5,                    KC_6, KC_7,         KC_8,         KC_9,         KC_0,            KC_GRV,
   KC_TAB,  KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                    KC_Y, KC_U,         KC_I,         KC_O,         KC_P,            KC_LBRC,
-  KC_NUHS, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                    KC_H, RSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_SCLN), KC_NUHS,
+  KC_NUHS, LGUI_T(KC_A), LALT_T(KC_S), LCTL_T(KC_D), KC_F,         KC_G,                    KC_H, KC_J,         LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_SCLN), KC_NUHS,
   KC_LSFT, KC_Z,         KC_X,         KC_C,         RALT_T(KC_V), KC_B, KC_DEL,  KC_RBRC,  KC_N, RALT_T(KC_M), KC_COMM,      KC_DOT,       KC_SLSH,         KC_QUOT,
-                                KC_LALT, MO(_LOWER), KC_ESC, KC_ENT,           KC_SPC, KC_BSPC, MO(_RAISE), KC_RALT
+                                KC_LALT, MO(_LOWER), KC_ESC, LSFT_T(KC_ENT),           RSFT_T(KC_SPC), KC_BSPC, MO(_RAISE), KC_RALT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -46,6 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |   _  |   +  |   {  |   }  |   |  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   |      |      |      | /(SHFT) /       \(SHFT)\  |      |      |      |
  *                   | LAlt | LOWER|Del   | /Enter  /       \Space \  |BackSP| RAISE| RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
@@ -67,6 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|   +  |   -  |   =  |   [  |   ]  |   \  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   |      |      |      | /(SHFT) /       \(SHFT)\  |      |      |      |
  *                   | LAlt | LOWER|Del   | /Enter  /       \Space \  |BackSP| RAISE| RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
@@ -90,6 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *                   |      |      |      | /(SHFT) /       \(SHFT)\  |      |      |      |
  *                   | LAlt | LOWER|Del   | /Enter  /       \Space \  |BackSP| RAISE| RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
@@ -153,5 +157,4 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
   return true;
 }
-
 
