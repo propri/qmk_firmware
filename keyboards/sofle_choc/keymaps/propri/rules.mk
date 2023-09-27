e95178fc6b31803597df09467a1930092b568a74
Wilhelm Mailaender <wilhelm.mailaender@avenga.com>
2023-09-27 09:54:59 +0200

    Erste Version Sofle keymap (nur 'qwerty' layer)

---
+++ keyboards/sofle_choc/keymaps/propri/rules.mk
@@ -0,0 +1,19 @@
+TRI_LAYER_ENABLE = yes
+
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+
+OLED_ENABLE = no
+
+# Enable per-key lighting
+RGB_MATRIX_ENABLE = no
+
+# Enable audio keys and system control
+EXTRAKEY_ENABLE = yes
+
+# Enable link time optimization
+# Trade longer compile time for smaller firmware size
+LTO_ENABLE = yes
+
+# Enable combos
+COMBO_ENABLE = yes
